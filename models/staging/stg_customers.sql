900476ad34358e6c547ba7db00eb5907cf99a349
mehdimediouni <memehdy.5@gmail.com>
2026-03-19 13:25:14 +0100

    fix : nettoyage

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,10 @@
+with customers as (
+
+select customer_id,
+       customer_unique_id,
+       customer_zip_code_prefix as zip_code,
+       customer_city as city,
+       customer_state as state
+from {{ source('olist_raw', 'customers') }}
+)
+select * from customers
---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,15 @@
+with source as (
+    select * from {{ source('olist_raw', 'payments') }}
+),
+
+renamed as (
+    select
+        order_id,
+        payment_sequential as payment_sequence,
+        payment_type,
+        payment_installments,
+        cast(payment_value as float) as payment_amount
+    from source
+)
+
+select * from renamed
