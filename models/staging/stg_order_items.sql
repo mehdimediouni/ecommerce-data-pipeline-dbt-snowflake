ab48fe9ee85a7ffd4fc962a9d871243dd66aa105
mehdimediouni <memehdy.5@gmail.com>
2026-04-02 18:58:53 +0200

    timestap tranformed to date

--- models/staging/stg_order_items.sql
+++ models/staging/stg_order_items.sql
@@ -8,7 +8,7 @@ renamed as (
         order_item_id,
         product_id,
         seller_id,
-        cast(shipping_limit_date as timestamp) as shipping_limit_at,
+        cast(shipping_limit_date as date) as shipping_limit_at,
         cast(price as float) as price,
         cast(freight_value as float) as shipping_cost,
         _loaded_at
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -7,11 +7,11 @@ renamed as (
         order_id,
         customer_id,
         order_status,
-        cast(order_purchase_timestamp as timestamp) as purchase_at,
-        cast(order_approved_at as timestamp) as approved_at,
-        cast(order_delivered_carrier_date as timestamp) as delivered_carrier_at,
-        cast(order_delivered_customer_date as timestamp) as delivered_customer_at,
-        cast(order_estimated_delivery_date as timestamp) as estimated_delivery_at,
+        cast(order_purchase_timestamp as date) as purchase_at,
+        cast(order_approved_at as date) as approved_at,
+        cast(order_delivered_carrier_date as date) as delivered_carrier_at,
+        cast(order_delivered_customer_date as date) as delivered_customer_at,
+        cast(order_estimated_delivery_date as date) as estimated_delivery_at,
         _loaded_at
     from source
 )
