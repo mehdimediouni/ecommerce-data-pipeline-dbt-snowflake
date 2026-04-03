d4f2050afe0e244a22d1651e604800698363b53d
mehdimediouni <memehdy.5@gmail.com>
2026-04-03 20:18:30 +0200

    fct orders updated

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -12,19 +12,31 @@ payments as (
 
 final as (
     select
+        -- Identifiants
         o.order_id,
         o.customer_id,
         o.order_status,
-        o.order_purchase_timestamp,
         
-        -- On ramène les infos des articles (depuis intermediate)
+        -- Dates (déjà castées en DATE dans ton staging)
+        o.purchase_date,
+        o.approved_date,
+        o.delivered_customer_date,
+        o.estimated_delivery_date,
+
+        -- Performance Logistique (importées de ton staging)
+        o.days_to_approve,
+        o.actual_delivery_duration,
+        o.delivery_delay,
+        o.is_late_delivery,
+
+        -- Finances & Articles (depuis intermediate items)
         i.total_items_price,
         i.total_shipping_cost,
         i.total_order_amount,
         i.nb_items,
         
-        -- On ramène les infos des paiements (depuis intermediate)
-        p.total_payment_amount,
+        -- Paiements (depuis intermediate payments)
+        p.total_payment_amount, 
         p.nb_installments,
         p.nb_payments,
         p.has_credit_card_payment,
